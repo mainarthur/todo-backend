584415b54cbdd8bf1df971bc54c4915d0549f5ef
Arthur Kh <listbox6@gmail.com>
2021-02-19 11:53:55 +0200

    beta release

---
+++ __tests__/api/api.js
@@ -0,0 +1,161 @@
+const frisby = require("frisby")
+const { Joi } = frisby
+
+const link = (endpoint) => `http://api.todolist.local/${endpoint}`
+
+
+describe("ToDos", function () {
+    it(`GET ${link("todo/all")}`, function () {
+        return frisby.get(link("todo/all"))
+            .expect("status", 200)
+            .expect('header', 'Content-Type', 'application/json')
+            .expect("jsonTypes", {
+                status: Joi.boolean(),
+                results: Joi.array()
+            })
+    })
+
+    it(`POST ${link("todo")}`, function () {
+        return frisby.post(link("todo"), {
+            text: "new todo"
+        })
+            .expect("status", 200)
+            .expect('header', 'Content-Type', 'application/json')
+            .expect("json", "result", {
+                text: "new todo"
+            })
+    })
+
+    it(`POST ${link("todo")} and check result with GET ${link("todo/all")}`, function () {
+        return frisby.post(link("todo"), {
+            text: "new todo"
+        })
+            .expect("status", 200)
+            .expect('header', 'Content-Type', 'application/json')
+            .expect("json", "result", {
+                text: "new todo"
+            }).then((newTodo) => {
+                let id = newTodo.json.result.id
+                return frisby.get(link("todo/all"))
+                    .expect("status", 200)
+                    .expect('header', 'Content-Type', 'application/json')
+                    .expect("jsonTypes", {
+                        status: Joi.boolean(),
+                        results: Joi.array()
+                    }).then((todos) => {
+                        expect(todos.json.results.find((todo) => todo.id == id)).toBeDefined()
+                    })
+            })
+    })
+
+    it(`POST ${link("todo")} and check result with GET ${link("todo")}`, function () {
+        return frisby.post(link("todo"), {
+            text: "new todo"
+        })
+            .expect("status", 200)
+            .expect('header', 'Content-Type', 'application/json')
+            .expect("json", "result", {
+                text: "new todo"
+            }).then((newTodo) => {
+                let id = newTodo.json.result.id
+                return frisby.get(link(`todo?id=${id}`))
+                    .expect("status", 200)
+                    .expect('header', 'Content-Type', 'application/json')
+                    .expect("json", newTodo.json)
+            })
+    })
+
+    it(`DELETE ${link("todo")}`, function() {
+        return frisby.post(link("todo"), {
+            text: "new todo"
+        })
+            .expect("status", 200)
+            .expect('header', 'Content-Type', 'application/json')
+            .expect("json", "result", {
+                text: "new todo"
+            }).then((newTodo) => {
+                let id = newTodo.json.result.id
+                return frisby.get(link("todo/all"))
+                    .expect("status", 200)
+                    .expect('header', 'Content-Type', 'application/json')
+                    .expect("jsonTypes", {
+                        status: Joi.boolean(),
+                        results: Joi.array()
+                    }).then((todos) => {
+                        expect(todos.json.results.find((todo) => todo.id == id)).toBeDefined()
+
+                        return frisby.del(link(`todo?id=${id}`))
+                            .expect("status", 200)
+                            .expect('header', 'Content-Type', 'application/json')
+                            .expect("jsonTypes", {
+                                status: Joi.boolean(),
+                                results: Joi.array()
+                            }).then((deletedToDo) => {
+                                let deletedToDoId = deletedToDo.json.results[0].id
+                                expect(deletedToDoId).toEqual(id)
+
+                                return frisby.get(link("todo/all"))
+                                .expect("status", 200)
+                                .expect('header', 'Content-Type', 'application/json')
+                                .expect("jsonTypes", {
+                                    status: Joi.boolean(),
+                                    results: Joi.array()
+                                }).then((todos) => {
+                                    expect(todos.json.results.find((todo) => todo.id == id)).toBeUndefined()
+                                })
+                            })
+
+                    })
+            })
+    })
+
+    it(`PATCH ${link("todo")}`, function() {
+        return frisby.post(link("todo"), {
+            text: "new todo"
+        })
+            .expect("status", 200)
+            .expect('header', 'Content-Type', 'application/json')
+            .expect("json", "result", {
+                text: "new todo"
+            }).then((newTodo) => {
+                let id = newTodo.json.result.id
+                return frisby.get(link("todo/all"))
+                    .expect("status", 200)
+                    .expect('header', 'Content-Type', 'application/json')
+                    .expect("jsonTypes", {
+                        status: Joi.boolean(),
+                        results: Joi.array()
+                    }).then((todos) => {
+                        expect(todos.json.results.find((todo) => todo.id == id)).toBeDefined()
+
+                        return frisby.patch(link(`todo`), {
+                            id,
+                            text: "updated text"
+                        })
+                            .expect("status", 200)
+                            .expect('header', 'Content-Type', 'application/json')
+                            .expect("json", {
+                                status: true,
+                                result: {
+                                    id,
+                                    text: "updated text"
+                                }
+                            }).then((updatedToDo) => {
+                                let updatedToDoId = updatedToDo.json.result.id
+                                expect(updatedToDoId).toEqual(id)
+
+                                return frisby.get(link("todo/all"))
+                                .expect("status", 200)
+                                .expect('header', 'Content-Type', 'application/json')
+                                .expect("jsonTypes", {
+                                    status: Joi.boolean(),
+                                    results: Joi.array()
+                                }).then((todos) => {
+                                    expect(todos.json.results.find((todo) => todo.id == id && todo.text == "updated text")).toBeDefined()
+                                })
+                            })
+
+                    })
+            })
+    })
+})
