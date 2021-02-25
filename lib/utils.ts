99171dc8d3d280e7be1e210b27b0f9e668cb6dea
Arthur Kh <listbox6@gmail.com>
2021-02-25 16:28:12 +0200

    initital commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,7 @@
 const emailRegexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const nameRegexp = /^([a-z]{2,}|[а-яё]{2,})$/
 
-export function uuidv4() {
+export function uuidv4():string {
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
         let r = (Math.random() * 16) | 0,
         v = c == "x" ? r : (r & 0x3) | 0x8;
@@ -11,15 +11,15 @@ export function uuidv4() {
     });
 }
 
-export function isValidEmail(email) {
+export function isValidEmail(email: string):boolean {
     return emailRegexp.test(String(email).toLowerCase())
 }
 
-export function isValidName(name) {
+export function isValidName(name: string):boolean {
     return nameRegexp.test(name.toLowerCase())
 }
 
-export function isValidPassword(password) {
+export function isValidPassword(password: string):boolean {
     return password.match(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,255}$/) != null
 }
 
