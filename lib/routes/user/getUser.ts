668b25a2e1e8122bf5e0ec9185e61326013693d9
Arthur Kh <listbox6@gmail.com>
2021-02-26 14:02:25 +0200

    upd

--- lib/routes/user/getUser.ts
+++ lib/routes/user/getUser.ts
@@ -2,17 +2,14 @@
 
 import { ParameterizedContext, Request } from "koa"
 import { IRouterParamContext } from "koa-router"
-import ToDo, { ToDoDocument } from "../../models/ToDo"
 import { UserPayload } from "../auth/UserPayload"
-import { Schema } from "mongoose"
-import { UpdateToDoRequest } from "../requests/UpdateToDoRequest"
 import User from "../../models/User"
 
 /**
  * @param {import("koa").ParameterizedContext<any, import("koa-router").IRouterParamContext<any, {}>, any>} ctx
  */
 export default async function getUser(ctx: ParameterizedContext<any, IRouterParamContext<any, {}>, any>): Promise<void> {
-    const { request, state: { payload } }: { request: Request, state: { payload: UserPayload } } = ctx
+    const { state: { payload } }: { request: Request, state: { payload: UserPayload } } = ctx
     const { id: userId }: { id: string } = payload
 
     const user = await User.findById(userId)
