f710e4794b5c5007904ce54d108b6e51e7f07363
Arthur Kh <listbox6@gmail.com>
2021-03-18 14:11:26 +0200

    upd

--- lib/routes/requests/UpdateToDoRequest.ts
+++ lib/routes/requests/UpdateToDoRequest.ts
@@ -3,10 +3,7 @@ import { Types } from "mongoose"
 export interface UpdateToDoRequest {
   boardId: Types.ObjectId
   _id: Types.ObjectId
-  text: string
-  done: boolean
-  createdAt: Date
-  lastUpdate: number
-  position: number
-  deleted: boolean
+  text?: string
+  done?: boolean
+  position?: number
 }
